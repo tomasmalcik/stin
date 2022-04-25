118848f2911593eeb306b3b15f7e4ce2b5137161
Tomáš Malčík <tomas.malcik@tul.cz>
2022-04-25 19:22:38 +0200

    First test

--- server/__tests__/initialTest.js
+++ server/__tests__/initialTest.js
@@ -1,4 +1,4 @@
 test('Adding two numbers', async () => {
-    expect(add(5, 5)).toStrictEqual(10)
-    expect(add(100, 200)).toStrictEqual(300)
+    expect(5+5).toStrictEqual(10)
+    expect(100+200).toStrictEqual(300)
 })
