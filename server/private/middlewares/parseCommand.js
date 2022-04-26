78c8e6f05d60ee950523db880c061baf092a52de
tomasmalcik <tomas.malcik@tul.cz>
2022-04-26 11:57:56 +0200

    Initial setup of parser

---
+++ server/private/middlewares/parseCommand.js
@@ -0,0 +1,5 @@
+function parseCommand(req, res, next) {
+
+}
+
+module.exports = parseCommand;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,6 @@
 const router = require("express").Router()
+const commandParser = require("../private/middlewares/parseCommand");
+
 
 router.get("/api/test", (req, res) => {
     res.status(200).send("Working..");
@@ -7,5 +9,9 @@ router.get("/api/test", (req, res) => {
 router.post("/api/test", (req, res) => {
     res.status(200).json(req.body.hobbies[0].swimming);
 })
+
+router.get("/api/sendCommand:command", commandParser, (req, res) => {
+
+});
 
 module.exports = router
