38677d4e0aede3d228edde0c86d5008b2cbad392
tomasmalcik <tomas.malcik@tul.cz>
2022-04-30 15:31:08 +0200

    Fix test

--- server/__tests__/writeFileTests.js
+++ server/__tests__/writeFileTests.js
@@ -13,6 +13,9 @@ describe("Testing of writefile function",  () => {
 
     test("Should fail writing to a file, because it is in read-only mode", async () => {
         const path = "./private/files/writeTst.txt";
+        await fs.chmod(path, 0o400, () => {
+
+        });
         const res = await writeToFile(path, "test");
         expect(res).toBe(false);
     })
